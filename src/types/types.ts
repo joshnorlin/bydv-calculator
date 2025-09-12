e535ca646e92267afaf689dd396a3d7ebb5660a0
josh <jnorlin@outlook.com>
2025-09-12 16:06:14 -0700

    refactor(types): add treatment types and

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,4 +1,42 @@
 export type InputDataType = {
   location: 'sparec' | 'rusty' | 'warsaw'
-  plantingDate: 'sept-oct' | 'oct-nov' | 'nov-dec'
+  plantingDate: 'sept-oct' | 'oct-nov' | 'nov-dec' | null
 }
+
+/* ----------------------------------------------- */
+
+const HasNotPlantedTreatments = [
+  "cont",
+  "neon",
+  "fallApp",
+  "springApp",
+  "neonFallApp",
+  "neonSpringApp"
+]
+
+const HasPlantedTreatments = [
+  "cont",
+  "fallApp",
+  "springApp"
+]
+
+export const TreatmentList = {
+  HasNotPlantedTreatments,
+  HasPlantedTreatments
+}
+
+/* ----------------------------------------------- */
+
+export type ConfigType = {
+  "bushelPrice": number,
+  treatmentCostsPerAcre: {
+    [treatment: string]: number
+  },
+  treatmentProfitsPerAcre: {
+    [location: string]: {
+      [date: string]: {
+        [treatment: string]: number
+      }
+    }
+  }
+};
