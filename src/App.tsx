5809c9185d283b0e18a4bdb8ecf49597782c2523
josh <jnorlin@outlook.com>
2025-09-19 15:39:42 -0700

    feat: add redux store persistence into localStorage functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { ConfigProvider } from './context/configContext';
 import { Button } from '@mui/material';
 
+import { Provider } from 'react-redux';
+import { store, persistor } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
 function App() {
   return (
     <Provider store={store}>
-      <ConfigProvider>
-        <Button variant="contained">Hello world</Button>
-      </ConfigProvider>
+      <PersistGate persistor={persistor} loading={null}>
+        <ConfigProvider>
+          <Button variant="contained">Hello world</Button>
+        </ConfigProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userDecisionReducer from './userDecisionSlice';
 import recommendationsReducer from './recommendationsSlice';
 
+const userDecisionPersistConfig = {
+  key: 'userDecision',
+  storage,
+}
+
+const recommendationsPersistConfig = {
+  key: 'recommendations',
+  storage,
+}
+
+const persistedUserDecisionReducer = persistReducer(userDecisionPersistConfig, userDecisionReducer);
+const persistedRecommendationsReducer = persistReducer(recommendationsPersistConfig, recommendationsReducer);
+
 export const store = configureStore({
   reducer: {
-    userDecision: userDecisionReducer,
-    recommendations: recommendationsReducer,
+    userDecision: persistedUserDecisionReducer,
+    recommendations: persistedRecommendationsReducer,
   },
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
